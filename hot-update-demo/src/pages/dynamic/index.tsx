558f9ee6c78434f2c3e0f56cd1e653eeacbb8123
wuchangming <wuchangmingnice@163.com>
2022-03-07 14:37:25 +0800

    chore: 清理废弃代码

--- hot-update-demo/src/pages/dynamic/index.tsx
+++ hot-update-demo/src/pages/dynamic/index.tsx
@@ -1,67 +1,6 @@
 import React from 'react'
 import { Button, View } from '@tarojs/components'
-// import { Component, ElementType } from 'react'
-// import { taroPageLifecycleNames } from '../utils/taroPageLifecycleNames'
-// import Taro from '@tarojs/taro'
-
-// function createRemotePage(p: Promise<{ default: any }>) {
-//     return class extends Component {
-//         constructor(props) {
-//             super(props)
-//             p.then(res => {
-//                 const RemotePage = res.default
-//                 this.setState({
-//                     RemotePage,
-//                 })
-//             })
-//         }
-
-//         state: {
-//             RemotePage: ElementType | undefined
-//         } = {
-//             RemotePage: undefined,
-//         }
-
-//         hasHandleTaroPageLifecycle = false
-//         handleTaroPageLifecycle = (ref: Ref<ElementType>) => {
-//             if (this.hasHandleTaroPageLifecycle) {
-//                 return
-//             }
-//             this.hasHandleTaroPageLifecycle = true
-
-//             // 页面事件透传
-//             taroPageLifecycleNames.forEach(name => {
-//                 // 如果未设置分享，则取消分享
-//                 if (!ref?.['onShareAppMessage']) {
-//                     Taro.hideShareMenu()
-//                 }
-//                 ;(this as any)[name] = (...args: any) => {
-//                     return ref?.[name]?.(...args)
-//                 }
-//             })
-//         }
-//         // 强行触发分享事件，用户没有再关掉
-//         onShareAppMessage() {
-//             return {}
-//         }
-
-//         render = () => {
-//             const RemotePage = this.state.RemotePage
-//             return RemotePage ? (
-//                 <RemotePage
-//                     ref={ref => {
-//                         this.handleTaroPageLifecycle(ref)
-//                     }}
-//                 ></RemotePage>
-//             ) : (
-//                 <View>Loading...</View>
-//             )
-//         }
-//     }
-// }
-
 import { createRemotePage } from '../mini-hot/createRemotePage'
-// import RemotePage from './RemotePage'
 
 export default createRemotePage({
     getPage: async () => {
